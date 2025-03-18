f4568b649b957109c57e6d3356d73091bca08d7f
Nebiyu Daniel <nebiyu.daniel@stu.ocsb.ca>
2025-03-18 15:00:17 +0000

    I added a favicon, javascript and css

--- js/script.js
+++ js/script.js
@@ -1,5 +1,11 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2022 nebs All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: nebs
+// Created on: Mar 2025
 // This file contains the JS functions for index.html
+
+function myButtonClicked() {
+  // this function displays "Hello, World!"
+  
+  document.getElementById("hello-world").innerHTML = "I coded this in Javascript! I think the square root of 25 is 5?"
+}
